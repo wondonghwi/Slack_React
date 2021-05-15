fb273d5eb3979ee5980ee3a5f7b5accb9cbdac59
원동휘 <donghwi1289@gmail.com>
2021-05-15 22:16:08 +0900

    feat : webpack, babel 세팅, 폴더구조 세팅, back폴더 외부에서 받아오기(clone)

---
+++ alecture/layouts/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import loadable from '@loadable/component';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+//page단위로 코드스플리팅 진행 -> lodable이용
+const Login = loadable(() => import('@pages/Login'));
+const SignUp = loadable(() => import('@pages/SignUp'));
+
+const App = () => {
+  return (
+    <Switch>
+      <Redirect exact path="/" to="/login" />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={SignUp} />
+    </Switch>
+  );
+};
+
+export default App;

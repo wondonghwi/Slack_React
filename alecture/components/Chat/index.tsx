dd0ba07016f5a94417839879aa1395546ecbd221
원동휘 <donghwi1289@gmail.com>
2021-05-30 10:46:37 +0900

    feat : regexifyString라이브러리를 이용해 정규식을 이용해 문자열 변환 기능구현

--- alecture/components/Chat/index.tsx
+++ alecture/components/Chat/index.tsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { memo, useMemo } from 'react';
 import { IDM } from '@typings/db';
 import { ChatWrapper } from '@components/Chat/styles';
 import gravatar from 'gravatar';
 import dayjs from 'dayjs';
+import regexifyString from 'regexify-string';
+import { Link } from 'react-router-dom';
+import { useParams } from 'react-router';
 
 interface ChatProps {
   data: IDM;
 }
 
 const Chat = ({ data }: ChatProps) => {
+  const { workspace } = useParams<{ workspace: string; channel: string }>();
   const user = data.Sender;
+
+  const result = useMemo(
+    () =>
+      regexifyString({
+        input: data.content,
+        pattern: /@\[(.+?)]\((\d+?)\)|\n/g,
+        decorator(match, index) {
+          const arr: string[] | null = match.match(/@\[(.+?)]\((\d+?)\)/)!;
+          if (arr) {
+            return (
+              <Link key={match + index} to={`/workspace/${workspace}/dm/${arr[2]}`}>
+                @{arr[1]}
+              </Link>
+            );
+          }
+          return <br key={index} />;
+        },
+      }),
+    [data.content, workspace],
+  );
+
   return (
     <ChatWrapper>
       <div className="chat-img">
@@ -20,10 +45,10 @@ const Chat = ({ data }: ChatProps) => {
           <b>{user.nickname}</b>
           <span>{dayjs(data.createdAt).format('h:mm A')}</span>
         </div>
-        <p>{data.content}</p>
+        <p>{result}</p>
       </div>
     </ChatWrapper>
   );
 };
 
-export default Chat;
+export default memo(Chat);
